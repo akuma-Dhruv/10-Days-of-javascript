1a2e38a55c815274ce4f0f03eb569eb8a522aec2
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-08-17 23:51:53 +0530

    hello world in js

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,5 @@
+//https://www.hackerrank.com/challenges/js10-hello-world/problem
+
 function greeting(parameterVariable) {
     // This line prints 'Hello, World!' to the console:
     console.log('Hello, World!');
