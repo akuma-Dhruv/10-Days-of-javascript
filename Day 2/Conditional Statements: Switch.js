c1ebae3597ffda56fc32c25b8c1cd06d3975fb59
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-08-19 19:02:18 +0530

    switch

---
+++ Day 2/Conditional Statements: Switch.js
@@ -0,0 +1,65 @@
+//https://www.hackerrank.com/challenges/js10-switch/problem
+
+function getLetter(s) {
+    let letter;
+    switch(s.charAt(0))
+    {
+        case 'a':letter='A';
+        break;
+        case 'e':letter='A';
+        break;
+        case 'i':letter='A';
+        break;
+        case 'o':letter='A';
+        break;
+        case 'u':letter='A';
+        break;
+        case 'b':letter='B';
+        break;
+        case 'c':letter='B';
+        break;
+        case 'd':letter='B';
+        break;
+        case 'f':letter='B';
+        break;
+        case 'g':letter='B';
+        break;
+        case 'h':letter='C';
+        break;
+        case 'j':letter='C';
+        break;
+        case 'k':letter='C';
+        break;
+        case 'l':letter='C';
+        break;
+        case 'm':letter='C';
+        break;
+        case 'n':letter='D';
+        break;
+        case 'p':letter='D';
+        break;
+        case 'q':letter='D';
+        break;
+        case 'r':letter='D';
+        break;
+        case 's':letter='D';
+        break;
+        case 't':letter='D';
+        break;
+        case 'v':letter='D';
+        break;
+        case 'w':letter='D';
+        break;
+        case 'x':letter='D';
+        break;
+        case 'y':letter='D';
+        break;
+        case 'z':letter='D';
+        break;
+        default: break;
+        
+        
+    }
+    
+    return letter;
+}
